f0e3dd21aad38a837ca93b7a9808f4bc76d66411
sobiyamoulvi <sobiyamoulvi299@gmail.com>
2019-11-21 14:46:42 +0530

    lines added by Sobiya....

--- py_second_git.py
+++ py_second_git.py
@@ -5,3 +5,6 @@ a=int(input("Enter number a:"))
 b=int(input("Enter number b:"))
 print("Add is:",a+b)
 print("Added lines throgh git hub account")
+
+#Adding lines to others code using Fork
+print("I am Sobiya....I  have Added lines to This File")
